f23e40728a50f284823bb89201d880b00bc1766b
Manuel Colmenero Navarro <manuelcolmenero@gmail.com>
2018-11-04 18:19:31 +0100

    Init window in AppDelegate Create MainViewController Call MainVC in AppDelegate

--- examples/AppDelegate.swift
+++ examples/AppDelegate.swift
@@ -15,7 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        let navigatorController: UINavigationController = UINavigationController()
+        let viewController: UIViewController = MainViewController()
+        
+        navigatorController.pushViewController(viewController, animated: true)
+        
+        window?.rootViewController = navigatorController
+        
         return true
     }
 
